0c6b32ff56e8421333deb9cfdcb49465c4e8c4fa
shrivatshan202 <shrivatshan202@gmail.com>
2026-04-17 16:22:47 +0530

    UC1: Display Consist Summary

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+/**
+ * Use Case 1: Initialize Train and Display Consist Summary
+ * This class represents the entry point of the Train Consist Management Application.
+ * Author: Garv
+ * Version: 1.0
+ */
+public class trainconsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully...");
+
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+
+        System.out.println("Current Train Consist: " + trainConsist);
+
+        System.out.println("System ready for operations...");
     }
 }

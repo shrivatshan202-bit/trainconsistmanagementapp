3a8fae78df532b55c17aa893fdb3988fa83ab1a1
shrivatshan202 <shrivatshan202@gmail.com>
2026-04-17 16:27:53 +0530

    UC2 - Add Passenger Bogies to Train

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Use Case 1: Initialize Train and Display Consist Summary
- * This class represents the entry point of the Train Consist Management Application.
- * Author: Garv
- * Version: 1.0
+ * Train Consist Management Application
+ * (App-Based Learning Using Core Java & Data Structures)
+ * * UC1: Initialize Train and Display Consist Summary
+ * UC2: Add Passenger Bogies to Train (ArrayList Operations)
+ * * Author: Garv
+ * Version: 2.0
  */
-public class trainconsistMgmt {
+public class trainconsistmanagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
-
         List<String> trainConsist = new ArrayList<>();
-
         System.out.println("Train initialized successfully...");
-
         System.out.println("Initial Bogie Count: " + trainConsist.size());
+        System.out.println("Current Train Consist: " + trainConsist + "\n");
 
-        System.out.println("Current Train Consist: " + trainConsist);
+        System.out.println("UC2 Add Passenger Bogies to Train");
 
-        System.out.println("System ready for operations...");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC Chair");
+        trainConsist.add("First Class");
+
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies: " + trainConsist);
+
+        trainConsist.remove("AC Chair");
+        System.out.println("After Removing 'AC Chair':");
+        System.out.println("Passenger Bogies: " + trainConsist);
+
+        System.out.println("Checking if 'Sleeper' exists:");
+        boolean hasSleeper = trainConsist.contains("Sleeper");
+        System.out.println("Contains Sleeper?: " + hasSleeper);
+
+        System.out.println("Final Train Passenger Consist:");
+        System.out.println(trainConsist);
+        System.out.println("UC2 operations completed successfully...");
     }
 }
